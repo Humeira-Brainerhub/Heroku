f4c840f4b35edce77b1b8c77de57692da23f58f0
Humeira <humeirap.brainerhub@gmail.com>
2022-06-15 17:51:55 +0530

    Added all files

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-app.get("/", function(req,res){
-    res.send("Working!!!")
-})
+const app = express();
+app.get("/", function (req, res) {
+  res.send("Working");
+});
+const dotenv = require("dotenv").config();
 
-app.listen(process.env.PORT || 5000)
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use('/uploads',express.static('uploads'));
+const db = require("./models");
+mongoose.connect(
+  db.url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+require("./routes/user")(app);
+
+app.listen(PORT, function () {
+  console.log("Listening on port " + PORT + ".");
+});
